01ade631e8c3fc293562b3a3e3cca0eefea0411a
hugohebbinckuys <hugo29.heb@gmail.com>
2025-11-24 10:45:55 +0100

    first commit

--- 1-hello-whale.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name "docker_class_exo3" -it --rm anthonyjhoiro/whalesay echo "Hello M1 Cyber 2025"
--- 2-python.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm --name "devops_class_exo2"  python:3
-
-# -it = mélage entre -i (interactive donc reçoit les entrées clavier) et -t (tty affiche les evenements (affiche le terminal etc.))
