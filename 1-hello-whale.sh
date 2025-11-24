d91012be6e147ab96c686e124a0be601f90d9208
hugohebbinckuys <hugo29.heb@gmail.com>
2025-11-24 11:02:23 +0100

    exo_1 + exo_2

---
+++ 1-hello-whale.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --name "retry_tp1" --rm -it anthonyjhoiro/whalesay cowsay "Hello M1 cyber 2025" 
---
+++ 2-python.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --name "retry_tp1_python" -it --rm python:3.13.9 python
